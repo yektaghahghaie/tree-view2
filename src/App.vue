1b3fcf4924e1fdfb61d37a9d09adb31de92ce416
yektaghahghaie <yktaqhqayy@gmail.com>
2023-08-14 11:45:49 +0330

    commit1

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<script>
+import TreeView from "./components/TreeView.vue";
+
+export default {
+  components: {
+    TreeView,
+  },
+  data() {
+    return {
+      node: {
+        id: 1,
+        label: "root",
+        children: [
+          {
+            id: 2,
+            label: "child 1",
+            children: [
+              {
+                id: 3,
+                label: "child 1.1",
+              },
+              {
+                id: 4,
+                label: "child 1.2",
+                children: [],
+              },
+            ],
+          },
+          {
+            id: 5,
+            label: "child 2",
+            children: [],
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    addFolder(node, label) {
+      console.log("addFolder", node, label);
+      // put your code here
+
+    
+    },
+    addFile(node, label) {
+      console.log("addFile", node, label);
+      // put your code here
+
+      
+    },
+    deleteNode(node) {
+      console.log("deleteNode", node);
+      // put your code here
+
+     
+    },
+
+    
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h2>Tree View</h2>
+    <TreeView
+      :node="node"
+      @addFolder="addFolder"
+      @addFile="addFile"
+      @deleteNode="deleteNode"
+    />
+  </main>
+</template>
+
+<style>
+main {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
